0ed2092b6323b251d6782a67eb92f2aa9b6825bd
JoshMusira <joshuamusira01@gmail.com>
2023-06-05 15:18:00 +0300

    Adding Functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,11 +9,16 @@ const Header = () => {
             <Navbar name="Text Analyzer" />
             <div className="main-navbar-elements">
                 {/* <Navbar name="About Us" /> */}
-                <Link id='Home'className='Link active' to="/">Home</Link>
+                <Link id='Home' className='Link active' to="/">Home</Link>
                 <Link id='about' className='Link' to="/about">About</Link>
                 <Link id='contact' className='Link' to="/contact">Contact</Link>
                 {/* <Navbar name="Contact Us" /> */}
 
+            </div>
+            <div className="humberger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
         </div>
     )
--- src/Components/TextArea.jsx
+++ src/Components/TextArea.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import '../Styles/textarea.css'
 import Section from './Section';
 import AverageContainer from './AverageContainer';
 
 
+
 const TextArea = () => {
+    const textAreaInput = useRef(null)
+
+    useEffect(() => {
+        textAreaInput.current.focus();
+
+    }, [])
     const [text, setText] = useState("");
     const HandleChange = (e) => {
         setText(e.target.value);
@@ -13,7 +20,7 @@ const TextArea = () => {
     return (
         <div className='text-area' >
             <Section text={text} />
-            <textarea onChange={HandleChange} name="" id="textarea-container" placeholder='Paste your text here ...' value={text} ></textarea>
+            <textarea onChange={HandleChange} name="" ref={textAreaInput} id="textarea-container" placeholder='Paste your text here ...' value={text} ></textarea>
             <AverageContainer text={text} />
         </div>
     )
